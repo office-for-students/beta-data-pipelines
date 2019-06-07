682f0c637c32d65506611aff9e6fbc0e8125a362
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-06-07 11:22:19 +0100

    Add function to extract HESA URL from POST req

---
+++ IngestRawXmlHttpTrigger/utils.py
@@ -0,0 +1,17 @@
+import logging
+
+
+def get_url_from_req(req):
+    """Returns the value of resource_url from the HTTP request"""
+    try:
+        req_body = req.get_json()
+        url = req_body["resource_url"]
+    except (ValueError, KeyError):
+        raise MissingUrlError
+
+    return url
+
+
+class MissingUrlError(Exception):
+   """Raised when the POST body does not contain a JSON encoded URL value"""
+   pass
