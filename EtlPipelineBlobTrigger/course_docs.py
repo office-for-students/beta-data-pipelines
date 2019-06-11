d0019db88261ebe4c44fc43c7b52bac4a3eb44c9
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-06-11 15:02:01 +0100

    Updates in light of updated course data doc

--- EtlPipelineBlobTrigger/course_docs.py
+++ EtlPipelineBlobTrigger/course_docs.py
@@ -10,7 +10,6 @@ import uuid
 import xmltodict
 import xml.etree.ElementTree as ET
 
-import sys # debug onlu
 def get_uuid():
     id = uuid.uuid1()
     return(id.hex)
@@ -38,14 +37,13 @@ def build_foundation_year_availability(raw_inst_data, raw_course_data):
 
 def build_length_of_course(raw_course_data):
     length_of_course_lookup = {
-        '0': 'todo',
-        '1': 'todo',
-        '2': 'todo',
-        '3': 'todo',
-        '4': 'todo',
-        '5': 'todo',
-        '6': 'todo',
-        '7': 'todo',
+        '1': '1 stage',
+        '2': '2 stages',
+        '3': '3 stages',
+        '4': '4 stages',
+        '5': '5 stages',
+        '6': '6 stages',
+        '7': '7 stages',
     }
 
     code = raw_course_data['NUMSTAGE']
@@ -62,14 +60,13 @@ def build_distance_learning(raw_inst_data, raw_course_data):
 
     code = raw_course_data['DISTANCE']
 
-    distance_learning = {
+    return {
         'code': raw_course_data['DISTANCE'],
         'label': distance_learning_lookup[code],
     }
-    return distance_learning
 
 
-def build_course(raw_inst_data, raw_course_data):
+def build_country(raw_inst_data, raw_course_data):
     country_code_lookup = {
         'XF': 'England',
         'XG': 'Northern Ireland',
@@ -77,25 +74,24 @@ def build_course(raw_inst_data, raw_course_data):
         'XI': 'Wales',
     }
 
-    course_section = {}
+    country_section = {}
 
-    # Need to check as Schema states this is optional
-    if 'UKPRNAPPLY' in raw_course_data:
-        course_section['application_provider'] = raw_course_data['UKPRNAPPLY']
-
-    country_code = raw_inst_data['COUNTRY']
-    country_name = country_code_lookup[country_code]
-    course_section['country'] = {'code': country_code, 'name': country_name}
-    return course_section
+    code = raw_inst_data['COUNTRY']
+    return {'code': code, 'name': country_code_lookup[code]}
 
 
 def build_output_course(raw_inst_data, raw_course_data):
-    output_course = {}
-    output_course['id'] = get_uuid()
-    output_course['created_at'] = datetime.datetime.utcnow().isoformat()
-    output_course['version'] = 1
+    course_entry = {}
+    course_entry['id'] = get_uuid()
+    course_entry['created_at'] = datetime.datetime.utcnow().isoformat()
+    course_entry['version'] = 1
 
-    output_course['course'] = build_course(raw_inst_data, raw_course_data)
+    output_course = {}
+    if 'UKPRNAPPLY' in raw_course_data:
+        output_course['application_provider'] = raw_course_data['UKPRNAPPLY']
+
+    output_course['country'] = build_country(raw_inst_data, raw_course_data)
+
     if 'DISTANCE' in raw_course_data:
         output_course['distance_learning'] = build_distance_learning(raw_inst_data, raw_course_data)
     if 'FOUNDATION' in raw_course_data:
@@ -104,13 +100,15 @@ def build_output_course(raw_inst_data, raw_course_data):
         output_course['honours_award_provision'] = raw_course_data['HONOURS']
     output_course['institution'] = build_institution(raw_inst_data)
     output_course['kis_course_id'] = raw_course_data['KISCOURSEID']
-    if 'NUM_STAGE' in raw_course_data:
+    if 'NUMSTAGE' in raw_course_data:
         output_course['length_of_course'] = build_length_of_course(raw_course_data)
-    return output_course
+
+    course_entry['course'] = output_course
+    return course_entry
 
 
-#fname = "../test-data/test-3-inst.xml"
-fname = "kis.xml"
+fname = "../test-data/test-3-inst.xml"
+#fname = "kis.xml"
 tree = ET.parse(fname)
 root = tree.getroot()
 
@@ -119,10 +117,10 @@ for institution in root.iter('INSTITUTION'):
     raw_inst_data = xmltodict.parse(ET.tostring(institution))['INSTITUTION']
     for course in institution.findall('KISCOURSE'):
         raw_course_data = xmltodict.parse(ET.tostring(course))['KISCOURSE']
-        output_course = build_output_course(raw_inst_data, raw_course_data)
-        print(json.dumps(output_course, indent=4))
+        course_entry = build_output_course(raw_inst_data, raw_course_data)
+        print(json.dumps(course_entry, indent=4))
         course_count += 1
-        #if course_count > 10:
-        #    break
+        if course_count > 10:
+            break
 
 print(f'Processed {course_count} courses')
