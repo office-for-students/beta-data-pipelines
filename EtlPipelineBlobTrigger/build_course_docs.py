d44c17f7b0bce20e993eb1478f48f9addf4e3703
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-06-13 10:24:59 +0100

    Various refactorings

--- EtlPipelineBlobTrigger/build_course_docs.py
+++ EtlPipelineBlobTrigger/build_course_docs.py
@@ -9,6 +9,7 @@ import uuid
 import xmltodict
 import xml.etree.ElementTree as ET
 import sys
+import course_lookup_tables as lookup
 
 from locations import Locations
 
@@ -26,82 +27,36 @@ def build_institution(raw_inst_data):
         "ukprn": raw_inst_data['UKPRN']
     }
 
-
-def build_foundation_year_availability(raw_inst_data, raw_course_data):
-    code = raw_course_data['FOUNDATION']
-    foundation_year_availability = {
-        '0': 'Not available',
-        '1': 'Optional',
-        '2': 'Compulsory',
-    }
-
-    code = raw_course_data['FOUNDATION']
-    label = foundation_year_availability[code]
-
-    return {'code': code, 'label': label}
-
-
-def build_length_of_course(raw_course_data):
-    length_of_course_lookup = {
-        '1': '1 stage',
-        '2': '2 stages',
-        '3': '3 stages',
-        '4': '4 stages',
-        '5': '5 stages',
-        '6': '6 stages',
-        '7': '7 stages',
-    }
-
-    code = raw_course_data['NUMSTAGE']
-    label = length_of_course_lookup[code]
-    return {'code': code, 'label': label}
-
-
-def build_distance_learning(raw_inst_data, raw_course_data):
-    distance_learning_lookup = {
-        '0': 'Course is availble other than by distance learning',
-        '1': 'Course is only availble through distance learning',
-        '2': 'Course is optionally available through distance learning',
-    }
-
-    code = raw_course_data['DISTANCE']
-
-    return {
-        'code': raw_course_data['DISTANCE'],
-        'label': distance_learning_lookup[code],
-    }
-
-
 def build_country(raw_inst_data):
-    country_code_lookup = {
-        'XF': 'England',
-        'XG': 'Northern Ireland',
-        'XH': 'Scotland',
-        'XI': 'Wales',
-    }
-
-    code = raw_inst_data['COUNTRY']
-    return {'code': code, 'name': country_code_lookup[code]}
+    entry = {}
+    if 'COUNTRY' in raw_inst_data:
+        code = raw_inst_data['COUNTRY']
+        entry['code'] = code 
+        entry['name'] = lookup.country_code[code]
+    return entry
 
 
-def get_locids(raw_course_data):
-    """Return a list of location IDs"""
+def get_locids(raw_course_data, ukprn):
+    """Returns a list of lookup keys for use in the locations class"""
     locids = []
     if 'COURSELOCATION' not in raw_course_data:
         return locids
     if type(raw_course_data['COURSELOCATION']) == list:
         for val in raw_course_data['COURSELOCATION']:
             # TODO check if UCASCOURSEIDs could be in here
-            # if so need skip over so wil
+            # if so need skip over as mean distant learning.
+            # we think. We should check that distant learning is set on
+            # the course level if this is the case.
             try:
-                locids.append(val['LOCID'])
+                locids.append(f"{val['LOCID']}{ukprn}")
             except KeyError:
                 # Handle COURSELOCATION without LOCID - see KISCOURSEID BADE for an example
                 # Distant learning may provide a UCASCOURSEID under COURSELOCATION
                 pass
     else:
         try:
-            locids.append(raw_course_data['COURSELOCATION']['LOCID'])
+            locids.append(
+                f"{raw_course_data['COURSELOCATION']['LOCID']}{ukprn}")
         except KeyError:
             # Handle COURSELOCATION without LOCID - see KISCOURSEID BADE for an example
             pass
@@ -109,7 +64,7 @@ def get_locids(raw_course_data):
 
 
 def build_accomodation_links(locations, locids):
-    """Needs to return a list"""
+    """Returns a list of accomodation links"""
     accom = []
     for locid in locids:
         accom_dict = {}
@@ -123,51 +78,72 @@ def build_accomodation_links(locations, locids):
     return accom
 
 
-def build_assesment_method_links(raw_course_data):
-    assessment_method = {}
-    if 'ASSURL' in raw_course_data:
-        assessment_method['english'] = raw_course_data['ASSURL']
-    if 'ASSURLW' in raw_course_data:
-        assessment_method['welsh'] = raw_course_data['ASSURLW']
-    return assessment_method
-
-
-def build_link_item(key, raw_course_data):
-    links = {}
+def build_link_item(key, lookup_table):
+    item = {}
     keyw = key + 'W'
-    if key in raw_course_data:
-        links['english'] = raw_course_data[key]
-    if keyw in raw_course_data:
-        links['welsh'] = raw_course_data[keyw]
-    return links
+    if key in lookup_table:
+        item['english'] = lookup_table[key]
+    if keyw in lookup_table:
+        item['welsh'] = lookup_table[keyw]
+    return item
 
 
 def build_links(locations, locids, raw_inst_data, raw_course_data):
     links = {}
+
     if locids:
+        # Handle accomodation separately to put in array array
         accomodation = build_accomodation_links(locations, locids)
         links['accomodation'] = accomodation
-    assessment_method = build_assesment_method_links(raw_course_data)
-    if assessment_method:
-        links['assessment_method'] = assessment_method
 
-    item_details = [('CRSEURL','course_page'),
-            ('EMPLOYURL', 'employment_details'),
-            ('SUPPORTURL','financial_support_details'),
-            ('LTURL', 'learning_and_teaching_methods')]
+    item_details = [('ASSURL', 'assesment_method', raw_course_data),
+                    ('CRSEURL', 'course_page', raw_course_data),
+                    ('EMPLOYURL', 'employment_details', raw_course_data),
+                    ('SUPPORTURL', 'financial_support_details', raw_course_data),
+                    ('LTURL', 'learning_and_teaching_methods', raw_course_data),
+                    ('SUURL', 'student_union', raw_inst_data)]
 
     for item_detail in item_details:
-        link_item = build_link_item(item_detail[0], raw_course_data)
+        link_item = build_link_item(item_detail[0], item_detail[2])
         if link_item:
             links[item_detail[1]] = link_item
-
-    #seld will be dynamically added here
-
 
     return links
 
 
+def build_location_items(locations, locids, raw_inst_data, raw_course_data):
+    """Returns a list of location items"""
+
+    location_items = []
+    for locid in locids:
+        location_dict = {}
+        raw_location_data = locations.get_location_data_for_locid(locid)
+        if 'LATITUDE' in raw_location_data:
+            location_dict['latitude'] = raw_location_data['LATITUDE']
+        if 'LONGITUDE' in raw_location_data:
+            location_dict['longitude'] = raw_location_data['LONGITUDE']
+        name = {}
+        if 'LOCNAME' in raw_location_data:
+            name['english'] = raw_location_data['LOCNAME']
+        if 'LOCNAMEW' in raw_location_data:
+            name['welsh'] = raw_location_data['LOCNAMEW']
+        if name:
+            location_dict['name']  = name
+        location_items.append(location_dict)
+    return location_items
+
+def build_code_label_entry(lookup_table_raw_xml, lookup_table_local, key):
+    """Generic function for building any code label entries"""
+    entry = {}
+    if key in lookup_table_raw_xml:
+        code = lookup_table_raw_xml[key]
+        entry['code'] = code
+        entry['label'] = lookup_table_local[code]
+    return entry
+
+
 def build_course_entry(locations, locids, raw_inst_data, raw_course_data):
+    """builds the JSON document for a particular course"""
     outer_wrapper = {}
     outer_wrapper['id'] = get_uuid()
     outer_wrapper['created_at'] = datetime.datetime.utcnow().isoformat()
@@ -176,24 +152,34 @@ def build_course_entry(locations, locids, raw_inst_data, raw_course_data):
     course_entry = {}
     if 'UKPRNAPPLY' in raw_course_data:
         course_entry['application_provider'] = raw_course_data['UKPRNAPPLY']
-    if 'COUNTRY' in raw_inst_data:
-        course_entry['country'] = build_country(raw_inst_data)
-    if 'DISTANCE' in raw_course_data:
-        course_entry['distance_learning'] = build_distance_learning(
-            raw_inst_data, raw_course_data)
-    if 'FOUNDATION' in raw_course_data:
-        course_entry['foundation_year_availability'] = build_foundation_year_availability(
-            raw_inst_data, raw_course_data)
+    country_entry = build_country(raw_inst_data)
+    if country_entry:
+        course_entry['country'] = country_entry 
+    distance_learning = build_code_label_entry(raw_course_data, lookup.distance_learning_lookup, 'DISTANCE')
+    if distance_learning:
+        course_entry['distance_learning'] = distance_learning
+    foundataion_year = build_code_label_entry(raw_course_data, lookup.foundation_year_availability, 'FOUNDATION')
+    if foundataion_year:
+        course_entry['foundation_year_availability'] = foundataion_year
     if 'HONOURS' in raw_course_data:
         course_entry['honours_award_provision'] = raw_course_data['HONOURS']
     course_entry['institution'] = build_institution(raw_inst_data)
     course_entry['kis_course_id'] = raw_course_data['KISCOURSEID']
-    if 'NUMSTAGE' in raw_course_data:
-        course_entry['length_of_course'] = build_length_of_course(
-            raw_course_data)
+    length_of_course = build_code_label_entry(raw_course_data, lookup.length_of_course, 'NUMSTAGE')
+    if length_of_course:
+        course_entry['length_of_course'] = length_of_course 
     links = build_links(locations, locids, raw_inst_data, raw_course_data)
     if links:
         course_entry['links'] = links
+    location_items = build_location_items(
+        locations, locids, raw_inst_data, raw_course_data)
+    if location_items:
+        course_entry['locations'] = location_items
+    mode = build_code_label_entry(raw_course_data, lookup.mode, 'KISMODE')
+    if mode:
+        course_entry['mode'] = mode
+    
+
     outer_wrapper['course'] = course_entry
     return outer_wrapper
 
@@ -209,10 +195,11 @@ course_count = 0
 for institution in root.iter('INSTITUTION'):
     raw_inst_data = xmltodict.parse(
         ET.tostring(institution))['INSTITUTION']
+    ukprn = raw_inst_data['UKPRN']
     for course in institution.findall('KISCOURSE'):
         raw_course_data = xmltodict.parse(ET.tostring(course))['KISCOURSE']
         """Change location to a list and just pass this rather than location data"""
-        locids = get_locids(raw_course_data)
+        locids = get_locids(raw_course_data, ukprn)
         #location = locations.get_location_data_for_locid(locid)
         course_entry = build_course_entry(
             locations, locids, raw_inst_data, raw_course_data)
--- EtlPipelineBlobTrigger/locations.py
+++ EtlPipelineBlobTrigger/locations.py
@@ -6,13 +6,23 @@ class Locations:
 
 
     def __init__(self, root):
-        """Build the locations lookup table"""
+        """Build the locations lookup table
+
+        Locations are unique on UKPRN and LOCID
+        """
+
+        used_location_keys = []
 
         self.lookup_dict = {}
         for location in root.iter('LOCATION'):
             raw_location_data = xmltodict.parse(ET.tostring(location))['LOCATION']
-            locid = raw_location_data['LOCID']
-            self.lookup_dict[locid] = raw_location_data
+            lockey = f"{raw_location_data['LOCID']}{raw_location_data['UKPRN']}"
+            if lockey in used_location_keys:
+                # Each location key should be unique according to schema
+                print("Key not unique !!!!")
+                sys.exit()
+            used_location_keys.append(lockey)
+            self.lookup_dict[lockey] = raw_location_data
 
     def get_location_data_for_locid(self, locid):
         """Return the location data for a locid or None if no data available"""
