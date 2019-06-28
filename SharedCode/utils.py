51aea2f419c8bbe78b68443c5a82a1112e756a2b
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-06-28 15:39:52 +0100

    Add get_collection_link

--- SharedCode/utils.py
+++ SharedCode/utils.py
@@ -4,10 +4,22 @@ import uuid
 import azure.cosmos.cosmos_client as cosmos_client
 
 
+
+def get_collection_link(db_id, collection_id):
+
+    # Get the relevant properties from Application Settings
+    cosmosdb_database_id = os.environ[db_id]
+    cosmosdb_collection_id = os.environ[collection_id]
+
+    #Return a link to the relevant CosmosDB Container/Document Collection
+    return 'dbs/' + cosmosdb_database_id + '/colls/' + cosmosdb_collection_id
+
 def get_cosmos_client():
-    # TODO run this over TLS
     cosmosdb_uri = os.environ['AzureCosmosDbUri']
     cosmosdb_key = os.environ['AzureCosmosDbKey']
+
+    # TODO inherited this, but need to check we are setting it correctly
+    # Should it have more entropy for example?
     master_key = 'masterKey'
 
     return cosmos_client.CosmosClient(url_connection=cosmosdb_uri, auth={master_key: cosmosdb_key})
