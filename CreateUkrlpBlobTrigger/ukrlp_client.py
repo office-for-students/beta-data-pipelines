34b2a545cb8f2eb580e72fb5a28de03755391575
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-01 08:16:03 +0100

    Improved variable names and comments

--- CreateUkrlpBlobTrigger/ukrlp_client.py
+++ CreateUkrlpBlobTrigger/ukrlp_client.py
@@ -1,14 +1,16 @@
-import requests
-import pprint
-import xmltodict
+"""This module contains the class that retrieves records from UKRLP"""
+
 import os
+import requests
+import xmltodict
+
 
 class UkrlpClient:
     """ Responsible for requesting data from UKRLP and returning data"""
 
     @staticmethod
     def get_soap_req(ukprn):
-        """Returns a SOAP request to query a specific UKRLP for a ukprn entry"""
+        """Returns a SOAP request to query a specific UKRLP for a ukprn"""
 
         soap_req = f"""
         <soapenv:Envelope
@@ -33,27 +35,26 @@ class UkrlpClient:
         """
         return soap_req
 
-
     @staticmethod
     def get_matching_provider_records(ukprn):
+        """Calls the UKRLP API to get records for the ukprn passed in"""
 
         url = os.environ['UkRlpUrl']
 
         headers = {'Content-Type': 'text/xml'}
 
         soap_req = UkrlpClient.get_soap_req(ukprn)
-        r = requests.post(url, data=soap_req, headers=headers)
-        status_code = r.status_code
-        if status_code != 200:
+        response = requests.post(url, data=soap_req, headers=headers)
+        if response.status_code != 200:
             return None
 
-        decoded_content = r.content.decode('utf-8')
+        decoded_content = response.content.decode('utf-8')
         enrichment_data_dict = xmltodict.parse(decoded_content)
 
         try:
-            provider_records = enrichment_data_dict['S:Envelope']['S:Body']['ns4:ProviderQueryResponse']['MatchingProviderRecords']
+            provider_records = enrichment_data_dict['S:Envelope']['S:Body'][
+                'ns4:ProviderQueryResponse']['MatchingProviderRecords']
         except KeyError:
             return None
-
 
         return provider_records
