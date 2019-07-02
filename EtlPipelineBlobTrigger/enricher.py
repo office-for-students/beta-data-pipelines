4669ccf4fa57cf5c9501fbf9dee2d1cce60a02dd
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-02 15:21:20 +0100

    Add a class to handle enriching course data

---
+++ EtlPipelineBlobTrigger/enricher.py
@@ -0,0 +1,23 @@
+from SharedCode import utils
+
+
+class CourseEnricher:
+    """Handles enriching courses with UKRLP data"""
+
+    def __init__(self):
+        self.ukrlp_lookups = utils.get_ukrlp_lookups()
+
+    def enrich_course(self, course):
+        """Takes a course and enriches ukprn names with UKRLP data"""
+
+        ukprn = course['course']['institution']['ukprn']
+        course['course']['institution']['ukprn_name'] = self.get_ukprn_name(
+            ukprn)
+
+        pub_ukprn = course['course']['institution']['pub_ukprn']
+        course['course']['institution'][
+            'pub_ukprn_name'] = self.get_ukprn_name(pub_ukprn)
+
+    def get_ukprn_name(self, ukprn):
+        """Returns a name for the ukprn"""
+        return self.ukrlp_lookups[ukprn].get('ukprn_name', 'not available')
