ff6644474ffd304b906244a886155cc2eec130cc
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-02 17:11:34 +0100

    initial update for copying blob

--- CreateUkrlpBlobTrigger/__init__.py
+++ CreateUkrlpBlobTrigger/__init__.py
@@ -9,17 +9,24 @@ import traceback
 
 import azure.functions as func
 
+from azure.storage.blob import BlockBlobService
+
 from .lookup_creator import LookupCreator
 
 def main(xmlblob: func.InputStream):
     """Creates the UKRLP lookup tables for later use
 
-
     This Azure Function carries out the following steps:
     * Decompresses the XML HESA DataSet
+
     * Parses the INSTITUTION data from the DataSet
+
     * Retrieves enrichment data from the UKRLP API for each institution
+
     * Creates a lookup item for each Institution and writes it to CosmosDB
+
+    * Currently, once completed successfully this function triggers the Etl function by copying
+      the compressed XML passed in to a Blob storage monitored by the Etl function. 
 
     """
 
@@ -28,7 +35,7 @@ def main(xmlblob: func.InputStream):
                      f"Name: {xmlblob.name}\n"
                      f"Blob Size: {xmlblob.length} bytes")
 
-        pipeline_start_datetime = datetime.today().strftime('%Y%m%d %H%M%S')
+        create_ukrlp_start_datetime = datetime.today().strftime('%Y%m%d %H%M%S')
         logging.info('CreateUkrlp function started on ' + pipeline_start_datetime)
 
         # Read the compressed Blob into a BytesIO object
@@ -48,6 +55,34 @@ def main(xmlblob: func.InputStream):
         # Parse the xml and create the lookups
         lookup_creator = LookupCreator(xml_string)
         lookup_creator.create_ukrlp_lookups()
+
+
+        #
+        # Copy the compressed HESA XML to the Blob storage monitored by Etl pipeline
+        #
+
+        storage_account_name = os.environ['AzureStorageAccountName']
+        storage_account_key = os.environ['AzureStorageAccountKey']
+        output_container_name = os.environ['EtlContainerName']
+        output_blob_name_prefix = os.environ['OutputBlobNamePrefix']
+        source_url = os.environ['UkrlpBlobUrl']
+
+        # Instantiate the Block Blob Service
+        blob_service = BlockBlobService(
+            account_name = storage_account_name, 
+            account_key = storage_account_key)
+        logging.info("Created Block Blob Service to Azure Storage Account '%s'", 
+            storage_account_name)
+
+        # Copy the dummy raw HESA XML from the dummy to the landing BLOB container
+        # Prefix the output BLOB name with the datetime it is being ingested
+
+        blob_service.copy_blob(
+            container_name = output_container_name, 
+            blob_name = output_blob_name_prefix + create_ukrlp_start_datetime + '-' + dummy_input_blob_name,
+            copy_source = source_url)
+        logging.info("Ingested raw HESA XML into Azure Storage Account Container '%s'", 
+            output_container_name)
 
         pipeline_end_datetime = datetime.today().strftime('%Y%m%d %H%M%S')
         logging.info('CreateUkrlp successfully finished on ' + pipeline_end_datetime)
