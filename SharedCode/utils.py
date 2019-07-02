2c83d93599f7503d025363bfad823a597a153f29
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-02 15:09:40 +0100

    Add function to get UKRLP lookups

--- SharedCode/utils.py
+++ SharedCode/utils.py
@@ -26,6 +26,21 @@ def get_cosmos_client():
     return cosmos_client.CosmosClient(url_connection=cosmosdb_uri,
                                       auth={master_key: cosmosdb_key})
 
-
 def get_uuid():
     return str(uuid.uuid1())
+
+def get_ukrlp_lookups():
+    """Returns a dictionary of UKRLP lookups"""
+
+    cosmos_client = get_cosmos_client()
+    collection_link = get_collection_link(
+                'AzureCosmosDbDatabaseId', 'AzureCosmosDbUkRlpCollectionId')
+
+    query = ( "SELECT * from c ")
+
+    options = {'enableCrossPartitionQuery': True}
+
+    lookup_list = list(
+        cosmos_client.QueryItems(collection_link, query, options))
+
+    return {lookup['ukprn']:lookup for lookup in lookup_list}
