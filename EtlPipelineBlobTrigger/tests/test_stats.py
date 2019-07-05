47992d50fb8bd4125ae366f2a8bf3086d22fff62
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-05 14:44:46 +0100

    Add test and fixtures for tesing continuation stats

--- EtlPipelineBlobTrigger/tests/test_stats.py
+++ EtlPipelineBlobTrigger/tests/test_stats.py
@@ -1,16 +1,29 @@
 import inspect
+import json
 import os
-import unittest
 import sys
+import unittest
 
-CURRENTDIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+import xmltodict
+
+CURRENTDIR = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
 PARENTDIR = os.path.dirname(CURRENTDIR)
-sys.path.insert(0,PARENTDIR)
+sys.path.insert(0, PARENTDIR)
 
 from course_stats import CourseStats
 
-class TestGetContinuationKey(unittest.TestCase):
 
+def get_string(filename):
+    """Reads file in test dir and returns a string"""
+
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(cwd, filename)) as fp:
+        xml_string = fp.read()
+    return xml_string
+
+
+class TestGetContinuationKey(unittest.TestCase):
     def setUp(self):
         self.course_stats = CourseStats()
 
@@ -20,12 +33,23 @@ class TestGetContinuationKey(unittest.TestCase):
         key = self.course_stats.get_continuation_key(xml_key)
         self.assertEqual(expected_key, key)
 
-
     def test_with_valid_key(self):
         invalid_xml_key = 'invalid_key'
         with self.assertRaises(KeyError):
             self.course_stats.get_continuation_key(invalid_xml_key)
 
+
+class TestCourseStatsGetContinuation(unittest.TestCase):
+    def setUp(self):
+        self.course_stats = CourseStats()
+
+    def test_course_stats_get_continuation(self):
+        raw_course_xml = xmltodict.parse(
+            get_string('fixtures/course_no_cont_subj.xml'))['KISCOURSE']
+        expected_response = json.loads(
+            get_string('fixtures/course_no_cont_subj_resp.json'))
+        continuation = self.course_stats.get_continuation(raw_course_xml)
+        self.assertListEqual(continuation, expected_response)
 
 
 if __name__ == '__main__':
