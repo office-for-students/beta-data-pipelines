17c770d8751131389368d8b59d0f34fb3920a092
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-11 10:24:38 +0100

    Move SharedUtils to bottom of module for easier reading

--- EtlPipelineBlobTrigger/course_stats.py
+++ EtlPipelineBlobTrigger/course_stats.py
@@ -25,96 +25,6 @@ class CourseStats:
         return stats
 
 
-class SharedUtils:
-    """Functionality required by several stats related classes"""
-
-    def __init__(self):
-        self.cwd = os.path.dirname(os.path.abspath(__file__))
-        self.subj_code_english = self.get_lookup('subj_code_english')
-        self.subj_code_welsh = self.get_lookup('subj_code_welsh')
-        self.unavail_reason = self.get_lookup('unavail_reason')
-
-    def get_lookup(self, lookup_name):
-        filename = {
-            'subj_code_english': 'subj_code_english.json',
-            'subj_code_welsh': 'subj_code_welsh.json',
-            'unavail_reason': 'contunavailreason.json'
-        }[lookup_name]
-        with open(os.path.join(self.cwd, f'lookup_files/{filename}')) as fp:
-            return json.load(fp)
-
-    def get_english_sbj_label(self, code):
-        return self.subj_code_english[code]
-
-    def get_welsh_sbj_label(self, code):
-        return self.subj_code_welsh[code]
-
-    def get_subject(self, elem, raw_subj_key):
-        subj_key = elem[raw_subj_key]
-        subject = {}
-        subject['code'] = subj_key
-        subject['english_label'] = self.get_english_sbj_label(subj_key)
-        subject['welsh_label'] = self.get_welsh_sbj_label(subj_key)
-        return subject
-
-    def get_unavailable_reason_subj(self, sbj_key):
-        if sbj_key:
-            return self.get_english_sbj_label(sbj_key)
-        return 'this subject'
-
-    def get_aggs_for_code(self, unavail_reason_code):
-        return self.unavail_reason['data'][unavail_reason_code].keys()
-
-    def need_unavailable(self, cont_elem, unavailreason_key, agg_key):
-        has_data = len(cont_elem) > 1
-        if not has_data:
-            return True
-
-        code = cont_elem[unavailreason_key]
-        agg = cont_elem[agg_key]
-        agg_codes = self.get_aggs_for_code(code)
-        if agg in agg_codes:
-            return True
-        return False
-
-    def get_unavailable_reason_str(self, unavail_reason_code, subj_key, agg,
-                                   xml_elem):
-        validate_unavailable_reason_code(unavail_reason_code)
-
-        has_data = len(xml_elem) > 1
-        if not has_data:
-            return self.unavail_reason['no-data'][unavail_reason_code]
-
-        validate_agg(unavail_reason_code, has_data, agg, self.unavail_reason)
-        partial_reason_str = self.unavail_reason['data'][unavail_reason_code][
-            agg]
-        partial_reason_str = unicodedata.normalize("NFKD", partial_reason_str)
-        subj = self.get_unavailable_reason_subj(subj_key)
-        return partial_reason_str + subj + '.'
-
-    def get_unavailable(self, cont_elem, raw_subj_key, raw_agg_key,
-                        raw_unavail_reason_key):
-        unavailable = {}
-        has_data = len(cont_elem) > 1
-        subj_key = cont_elem.get(raw_subj_key)
-        agg = cont_elem[raw_agg_key] if has_data else None
-        unavail_reason_code = cont_elem[raw_unavail_reason_key]
-        validate_unavailable_reason_code(unavail_reason_code)
-
-        unavailable['code'] = unavail_reason_code
-        unavailable['reason'] = self.get_unavailable_reason_str(
-            unavail_reason_code, subj_key, agg, cont_elem)
-        return unavailable
-
-    @staticmethod
-    def get_raw_list(raw_course_data, element_key):
-        """Get a list for the element"""
-
-        if isinstance(raw_course_data[element_key], dict):
-            return [raw_course_data[element_key]]
-        return raw_course_data[element_key]
-
-
 class Continuation:
     """Extracts and transforms the continuation course element"""
 
@@ -242,3 +152,93 @@ class Employment:
                 ordered_json_elem = OrderedDict(sorted(json_elem.items()))
             json_elem_list.append(ordered_json_elem)
         return json_elem_list
+
+
+class SharedUtils:
+    """Functionality required by several stats related classes"""
+
+    def __init__(self):
+        self.cwd = os.path.dirname(os.path.abspath(__file__))
+        self.subj_code_english = self.get_lookup('subj_code_english')
+        self.subj_code_welsh = self.get_lookup('subj_code_welsh')
+        self.unavail_reason = self.get_lookup('unavail_reason')
+
+    def get_lookup(self, lookup_name):
+        filename = {
+            'subj_code_english': 'subj_code_english.json',
+            'subj_code_welsh': 'subj_code_welsh.json',
+            'unavail_reason': 'contunavailreason.json'
+        }[lookup_name]
+        with open(os.path.join(self.cwd, f'lookup_files/{filename}')) as fp:
+            return json.load(fp)
+
+    def get_english_sbj_label(self, code):
+        return self.subj_code_english[code]
+
+    def get_welsh_sbj_label(self, code):
+        return self.subj_code_welsh[code]
+
+    def get_subject(self, elem, raw_subj_key):
+        subj_key = elem[raw_subj_key]
+        subject = {}
+        subject['code'] = subj_key
+        subject['english_label'] = self.get_english_sbj_label(subj_key)
+        subject['welsh_label'] = self.get_welsh_sbj_label(subj_key)
+        return subject
+
+    def get_unavailable_reason_subj(self, sbj_key):
+        if sbj_key:
+            return self.get_english_sbj_label(sbj_key)
+        return 'this subject'
+
+    def get_aggs_for_code(self, unavail_reason_code):
+        return self.unavail_reason['data'][unavail_reason_code].keys()
+
+    def need_unavailable(self, cont_elem, unavailreason_key, agg_key):
+        has_data = len(cont_elem) > 1
+        if not has_data:
+            return True
+
+        code = cont_elem[unavailreason_key]
+        agg = cont_elem[agg_key]
+        agg_codes = self.get_aggs_for_code(code)
+        if agg in agg_codes:
+            return True
+        return False
+
+    def get_unavailable_reason_str(self, unavail_reason_code, subj_key, agg,
+                                   xml_elem):
+        validate_unavailable_reason_code(unavail_reason_code)
+
+        has_data = len(xml_elem) > 1
+        if not has_data:
+            return self.unavail_reason['no-data'][unavail_reason_code]
+
+        validate_agg(unavail_reason_code, has_data, agg, self.unavail_reason)
+        partial_reason_str = self.unavail_reason['data'][unavail_reason_code][
+            agg]
+        partial_reason_str = unicodedata.normalize("NFKD", partial_reason_str)
+        subj = self.get_unavailable_reason_subj(subj_key)
+        return partial_reason_str + subj + '.'
+
+    def get_unavailable(self, cont_elem, raw_subj_key, raw_agg_key,
+                        raw_unavail_reason_key):
+        unavailable = {}
+        has_data = len(cont_elem) > 1
+        subj_key = cont_elem.get(raw_subj_key)
+        agg = cont_elem[raw_agg_key] if has_data else None
+        unavail_reason_code = cont_elem[raw_unavail_reason_key]
+        validate_unavailable_reason_code(unavail_reason_code)
+
+        unavailable['code'] = unavail_reason_code
+        unavailable['reason'] = self.get_unavailable_reason_str(
+            unavail_reason_code, subj_key, agg, cont_elem)
+        return unavailable
+
+    @staticmethod
+    def get_raw_list(raw_course_data, element_key):
+        """Get a list for the element"""
+
+        if isinstance(raw_course_data[element_key], dict):
+            return [raw_course_data[element_key]]
+        return raw_course_data[element_key]
