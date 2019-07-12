583bbffaee6b6093d4cf4f49a69f4fd7d9913946
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-12 14:36:17 +0100

    Add further tests for Entry statistics

--- EtlPipelineBlobTrigger/tests/test_entry.py
+++ EtlPipelineBlobTrigger/tests/test_entry.py
@@ -52,4 +52,25 @@ class TestGetEntry(unittest.TestCase):
             for course in institution.findall('KISCOURSE'):
                 raw_course_data = xmltodict.parse(ET.tostring(course))['KISCOURSE']
                 entry = self.entry.get_entry(raw_course_data)
-                print(json.dumps(entry, indent=4))
+
+    def test_get_entry_no_subj(self):
+        raw_course_xml = xmltodict.parse(
+            get_string('fixtures/course_no_subj_for_most.xml'))['KISCOURSE']
+        expected_response = json.loads(
+            get_string('fixtures/course_no_entry_subj_resp.json'))
+        json_obj = self.entry.get_entry(raw_course_xml)
+        self.assertListEqual(json_obj, expected_response)
+
+    def test_get_entry_with_subj(self):
+        raw_course_xml = xmltodict.parse(
+            get_string('fixtures/course_with_subj_for_most.xml'))['KISCOURSE']
+        expected_response = json.loads(
+            get_string('fixtures/course_entry_subj_resp.json'))
+        json_obj = self.entry.get_entry(raw_course_xml)
+        print(json.dumps(json_obj, indent=4))
+        self.assertListEqual(json_obj, expected_response)
+
+ # TODO Test more of the functionality - more lookups etc
+
+if __name__ == '__main__':
+    unittest.main()
