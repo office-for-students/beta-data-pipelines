a66a54cb3dc0ac533009ea69019a2ab4919c18ab
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-18 19:29:39 +0100

    Add initial test for top level get_stats

---
+++ EtlPipelineBlobTrigger/tests/test_get_stats.py
@@ -0,0 +1,34 @@
+import json
+import os
+import unittest
+import xml.etree.ElementTree as ET
+
+import xmltodict
+
+import course_stats
+
+def get_string(filename):
+    """Reads file into a string and returns"""
+
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(cwd, filename)) as fp:
+        string = fp.read()
+    return string
+
+
+class TestGetStats(unittest.TestCase):
+
+    def test_with_large_file(self):
+        """Initial smoke test"""
+        xml_string = get_string('fixtures/large-test-file.xml')
+        root = ET.fromstring(xml_string)
+        for institution in root.iter('INSTITUTION'):
+            for course in institution.findall('KISCOURSE'):
+                raw_course_data = xmltodict.parse(
+                    ET.tostring(course))['KISCOURSE']
+                course_stats.get_stats(raw_course_data)
+
+# TODO Test more of the functionality - more lookups etc
+
+if __name__ == '__main__':
+    unittest.main()
