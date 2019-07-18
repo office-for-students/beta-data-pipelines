62b8b6b4b2fae9c959d0ec20b750cd851c6973ca
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-18 16:13:44 +0100

    Add more tests

--- EtlPipelineBlobTrigger/tests/test_tariff.py
+++ EtlPipelineBlobTrigger/tests/test_tariff.py
@@ -22,25 +22,37 @@ class TestVariousHelperMethods(unittest.TestCase):
     def setUp(self):
         self.tariff = Tariff()
 
-    def test_is_tariff(self):
-        self.assertTrue(self.tariff.is_tariff('T001'))
-        self.assertTrue(self.tariff.is_tariff('T048'))
-        self.assertFalse(self.tariff.is_tariff('T999'))
-
     def test_get_tariff_description(self):
         self.assertEqual(self.tariff.get_tariff_description('T001'), 'less than 48 tariff points')
-        self.assertEqual(self.tariff.get_tariff_description('T208'), 'between 208 and 239 tariff points')
+        self.assertEqual(self.tariff.get_tariff_description('T048'), 'between 48 and 63 tariff points')
+        self.assertEqual(self.tariff.get_tariff_description('T064'), 'between 64 and 79 tariff points')
+        self.assertEqual(self.tariff.get_tariff_description('T080'), 'between 80 and 95 tariff points')
+        self.assertEqual(self.tariff.get_tariff_description('T096'), 'between 96 and 111 tariff points')
+        self.assertEqual(self.tariff.get_tariff_description('T208'), 'between 208 and 223 tariff points')
+        self.assertEqual(self.tariff.get_tariff_description('T224'), 'between 224 and 239 tariff points')
+        self.assertEqual(self.tariff.get_tariff_description('T240'), '240 or more tariff points')
 
-    def test_get_tariff_list(self):
+    def test_get_tariffs_list(self):
         raw_course_xml = xmltodict.parse(
             get_string('fixtures/course_tariff_with_subj.xml'))['KISCOURSE']
         raw_xml_list = SharedUtils.get_raw_list(raw_course_xml, 'TARIFF')
         tariff_xml_elem = raw_xml_list[0]
-        tariff_list = self.tariff.get_tariff_list(tariff_xml_elem)
+        tariff_list = self.tariff.get_tariffs_list(tariff_xml_elem)
         expected_result = json.loads(
-            get_string('fixtures/course_tariff_with_subj.json'))
+            get_string('fixtures/tariff_get_tariffs_list_resp.json'))
         self.assertListEqual(tariff_list, expected_result)
-        print(json.dumps(tariff_list, indent=4))
+
+class TestGetStats(unittest.TestCase):
+    def setUp(self):
+        self.tariff = Tariff()
+
+    def test_get_stats_with_subj(self):
+        raw_course_xml = xmltodict.parse(
+            get_string('fixtures/course_tariff_with_subj.xml'))['KISCOURSE']
+        expected_result = json.loads(
+            get_string('fixtures/tariff_get_stats_resp_with_subj.json'))
+        json_obj = self.tariff.get_stats(raw_course_xml)
+        self.assertListEqual(json_obj, expected_result)
 
 
 # TODO Test more of the functionality - more lookups etc
