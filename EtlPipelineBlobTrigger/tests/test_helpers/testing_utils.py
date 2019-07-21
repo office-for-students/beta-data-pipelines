3c3ab183a1ee643159fe9ede292024760502de71
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-21 13:14:58 +0100

    Make path building work from different locations

--- EtlPipelineBlobTrigger/tests/test_helpers/testing_utils.py
+++ EtlPipelineBlobTrigger/tests/test_helpers/testing_utils.py
@@ -2,11 +2,14 @@
 import os
 from pathlib import Path
 
+file_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(file_dir)
+
 def get_string(filename):
     """Reads file into a string and returns"""
 
     #parent_dir = str(Path().resolve().parent)
     cwd = Path().absolute()
-    with open(os.path.join(cwd, filename)) as infile:
+    with open(os.path.join(parent_dir, filename)) as infile:
         string = infile.read()
     return string
