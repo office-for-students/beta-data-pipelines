af91f2494b8d3aed78ee357fc56befd1dc643f3c
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-21 18:07:25 +0100

    Tidy up formatting and PEP8 related

--- EtlPipelineBlobTrigger/tests/test_helpers/testing_utils.py
+++ EtlPipelineBlobTrigger/tests/test_helpers/testing_utils.py
@@ -1,15 +1,12 @@
 """General purpose functions used in tests"""
 import os
-from pathlib import Path
 
-file_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(file_dir)
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(FILE_DIR)
+
 
 def get_string(filename):
     """Reads file into a string and returns"""
-
-    #parent_dir = str(Path().resolve().parent)
-    cwd = Path().absolute()
-    with open(os.path.join(parent_dir, filename)) as infile:
+    with open(os.path.join(PARENT_DIR, filename)) as infile:
         string = infile.read()
     return string
