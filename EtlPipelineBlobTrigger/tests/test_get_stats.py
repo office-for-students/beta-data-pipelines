777fde603dae728c99077ca54c3105b2d9e2820c
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-24 11:23:24 +0100

    Pass country_code to get_stats, use get_string from helper, and some formatting

--- EtlPipelineBlobTrigger/tests/test_get_stats.py
+++ EtlPipelineBlobTrigger/tests/test_get_stats.py
@@ -1,34 +1,31 @@
-import json
-import os
 import unittest
 import xml.etree.ElementTree as ET
 
 import xmltodict
 
 import course_stats
-
-def get_string(filename):
-    """Reads file into a string and returns"""
-
-    cwd = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(cwd, filename)) as fp:
-        string = fp.read()
-    return string
+from course_docs import get_country
+from testing_utils import get_string
 
 
 class TestGetStats(unittest.TestCase):
-
     def test_with_25_courses(self):
         """Initial smoke test"""
-        xml_string = get_string('fixtures/25_courses.xml')
+        xml_string = get_string("fixtures/25_courses.xml")
         root = ET.fromstring(xml_string)
-        for institution in root.iter('INSTITUTION'):
-            for course in institution.findall('KISCOURSE'):
-                raw_course_data = xmltodict.parse(
-                    ET.tostring(course))['KISCOURSE']
-                course_stats.get_stats(raw_course_data)
+        for institution in root.iter("INSTITUTION"):
+            raw_inst_data = xmltodict.parse(ET.tostring(institution))[
+                "INSTITUTION"
+            ]
+            country_code = get_country(raw_inst_data)["code"]
+            for course in institution.findall("KISCOURSE"):
+                raw_course_data = xmltodict.parse(ET.tostring(course))[
+                    "KISCOURSE"
+                ]
+                course_stats.get_stats(raw_course_data, country_code)
+
 
 # TODO Test more of the functionality
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
