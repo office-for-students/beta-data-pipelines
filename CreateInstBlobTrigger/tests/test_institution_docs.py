4e7c2673a6ce5f2910dbc8f5a52b1220693eb0cf
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-26 09:09:11 +0100

    Move helper function to another module add new test

--- CreateInstBlobTrigger/tests/test_institution_docs.py
+++ CreateInstBlobTrigger/tests/test_institution_docs.py
@@ -1,27 +1,29 @@
-import json
 import unittest
 
 import xml.etree.ElementTree as ET
 
-import xmltodict
-
 from institution_docs import get_total_number_of_courses
-from course_stats import Leo
 
-from testing_utils import get_string
+from testing_utils import get_string, get_first
 
 
 class TestStaticHelperFunctions(unittest.TestCase):
-    def test_get_total_number_of_courses(self):
 
-        def get_first(Node):
-            for x in Node:
-                return x
+    def test_get_total_number_of_courses_with_one_course(self):
 
         xml_string = get_string("fixtures/one_inst_one_course.xml")
         root = ET.fromstring(xml_string)
         institution = get_first(root.iter("INSTITUTION"))
         expected_number_of_courses = 1
+        number_of_courses = get_total_number_of_courses(institution)
+        self.assertEqual(expected_number_of_courses, number_of_courses)
+
+    def test_get_total_number_of_courses_with_nine_courses(self):
+
+        xml_string = get_string("fixtures/one_inst_nine_courses.xml")
+        root = ET.fromstring(xml_string)
+        institution = get_first(root.iter("INSTITUTION"))
+        expected_number_of_courses = 9
         number_of_courses = get_total_number_of_courses(institution)
         self.assertEqual(expected_number_of_courses, number_of_courses)
 
