e14ef7d577496d6c1320ff8dcb925f7fac262c55
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-26 15:08:43 +0100

    Fix indentation  and format

--- CreateInstBlobTrigger/__init__.py
+++ CreateInstBlobTrigger/__init__.py
@@ -7,36 +7,37 @@ import io
 import logging
 import os
 from datetime import datetime
-from distutils.util import strtobool
 
 import azure.functions as func
-from azure.storage.blob import BlockBlobService
 
-from institution_docs import InstitutionDocs
+from .institution_docs import InstitutionDocs
 from SharedCode import exceptions
+
 
 def main(xmlblob: func.InputStream, context: func.Context):
 
-    logging.info(f"CreateInstBlobTrigger Python BLOB trigger function processing BLOB \n"
-                 f"Name: {xmlblob.name}\n"
-                 f"Blob Size: {xmlblob.length} bytes")
-try:
+    logging.info(
+        f"CreateInstBlobTrigger processing BLOB \n"
+        f"Name: {xmlblob.name}\n"
+        f"Blob Size: {xmlblob.length} bytes"
+    )
+    try:
 
         """ 0. PREPARATION """
 
-        pipeline_start_datetime = datetime.today().strftime('%Y%m%d %H%M%S')
-
-        xsd_filename = os.environ['XsdFilename']
+        xsd_filename = os.environ["XsdFilename"]
         xsd_path = os.path.join(context.function_directory, xsd_filename)
 
-        logging.info(f"CreateInstBlobTrigger configuration values \n"
-                 f"XsdFilename: {xsd_filename}\n"
-                 f"XsdPath: {xsd_path}")
+        logging.info(
+            f"CreateInstBlobTrigger configuration values \n"
+            f"XsdFilename: {xsd_filename}\n"
+            f"XsdPath: {xsd_path}"
+        )
 
         """ 1. DECOMPRESSION - Decompress the compressed HESA XML """
-        # Note the HESA XML blob provided to this function will be gzip compressed.
+        # The XML blob provided to this function will be gzip compressed.
         # This is a work around for a limitation discovered in Azure,
-        # in that Functions written in Python do not get triggered
+        # where Functions written in Python do not get triggered
         # correctly with large blobs. Tests showed this is not a limitation
         # with Funtions written in C#.
 
@@ -50,32 +51,46 @@ try:
         decompressed_file = compressed_gzip.read()
 
         # Decode the bytes into a string
-        xml_string = decompressed_file.decode('utf-8')
-
+        xml_string = decompressed_file.decode("utf-8")
 
         """ 2. VALIDATION - Validate the HESA Raw XML against the XSD """
 
         # TODO fix failing validation.
         # validators.validate_xml(xsd_path, xml_string)
 
-        """ 3. LOADING - Parse XML and create enriched JSON Documents in Document Database """
+        """ 3. LOADING - extract data and create enriched JSON Documents """
 
-        institution_docs = InstitutionDocs()
-        institution_docs.create_institution_docs(xml_string)
+        inst_docs = InstitutionDocs()
+        inst_docs.create_institution_docs(xml_string)
 
         """ 4. CLEANUP """
 
-        pipeline_end_datetime = datetime.today().strftime('%Y%m%d %H%M%S')
-        logging.info('CreateInstBlobTrigger successfully finished on ' + pipeline_end_datetime)
+        pipeline_end_datetime = datetime.today().strftime("%Y%m%d %H%M%S")
+        logging.info(
+            "CreateInstBlobTrigger successfully finished on "
+            + pipeline_end_datetime
+        )
 
     except exceptions.StopEtlPipelineWarningException:
 
-        # A WARNING is raised during the ETL Pipeline and StopEtlPipelineOnWarning=True
-        # For example, the incoming raw XML is not valid against its XSD
-        error_message = 'A WARNING has been encountered during the ETL Pipeline. ' \
-            'The Pipeline will be stopped since StopEtlPipelineOnWarning has been ' \
-            'set to TRUE in the Application Settings.'
+        # A WARNING is raised during the ETL Pipeline and
+        # StopEtlPipelineOnWarning=True. For example, the incoming raw XML
+        # is not valid against its XSD
+        error_message = (
+            "A WARNING has been encountered during the ETL Pipeline. "
+            "The Pipeline will be stopped since StopEtlPipelineOnWarning is "
+            "set to TRUE in the Application Settings."
+        )
         logging.error(error_message)
-        pipeline_fail_datetime = datetime.today().strftime('%Y%m%d %H%M%S')
-        logging.error('ETL Pipeline failed on ' + pipeline_fail_datetime)
+        pipeline_fail_datetime = datetime.today().strftime("%Y%m%d %H%M%S")
+        logging.error("ETL Pipeline failed on " + pipeline_fail_datetime)
         raise Exception(error_message)
+
+    except Exception as e:
+        # Unexpected exception
+        logging.error(
+            "CreateInstBlogTrigger unexpected exception ", exc_info=True
+        )
+
+        # Raise to Azure
+        raise e
