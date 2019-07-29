bc7bf9c0b3a2461323b634e152eb10fb6199cba2
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-29 17:15:10 +0100

    Add test for get_website

---
+++ CreateUkrlpBlobTrigger/tests/test_lookup_creator.py
@@ -0,0 +1,26 @@
+import inspect
+import json
+import os
+import sys
+import unittest
+import xml.etree.ElementTree as ET
+
+from unittest import mock
+
+
+from lookup_creator import LookupCreator
+from ukrlp_test_utils import get_string
+
+
+class TestGetMatchingProviderRecords(unittest.TestCase):
+    def test_get_website_100000794(self):
+        matching_provider_records = json.loads(get_string("fixtures/ukrlp_10000794.json"))
+        expected_website = "www.boltoncollege.ac.uk"
+        website = LookupCreator.get_website(matching_provider_records)
+        self.assertEqual(expected_website, website)
+
+
+# TODO Test more of the functionality - more lookups etc
+
+if __name__ == "__main__":
+    unittest.main()
