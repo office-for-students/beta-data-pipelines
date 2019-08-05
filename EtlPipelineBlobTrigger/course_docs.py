0b9115b9958b2b2add2fe4585004cc20b9023e34
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-08-05 13:41:08 +0100

    Update get_accreditations to use common code and move to course docs

--- EtlPipelineBlobTrigger/course_docs.py
+++ EtlPipelineBlobTrigger/course_docs.py
@@ -23,12 +23,13 @@ sys.path.insert(0, CURRENTDIR)
 
 import course_lookup_tables as lookup
 from course_stats import get_stats, SharedUtils
-from course_accreditations import get_accreditations
 from accreditations import Accreditations
 from kisaims import KisAims
 from locations import Locations
 from SharedCode import utils
 from ukrlp_enricher import UkRlpCourseEnricher
+
+# TODO update this to use the new one that will be in SharedCode after PR#34
 from helpers import get_eng_welsh_item
 
 
@@ -178,6 +179,37 @@ def get_qualification(lookup_table_raw_xml, kisaims):
             entry['label'] = label
     return entry
 
+
+def get_accreditations(raw_course_data, acc_lookup):
+    acc_list = []
+    raw_xml_list = SharedUtils.get_raw_list(raw_course_data,
+                                            'ACCREDITATION')
+
+    for xml_elem in raw_xml_list:
+        json_elem = {}
+
+        if 'ACCTYPE' in xml_elem:
+            json_elem['type'] = xml_elem['ACCTYPE']
+            accreditations = acc_lookup.get_accreditation_data_for_key(
+                                xml_elem['ACCTYPE'])
+
+            if 'ACCURL' in accreditations:
+                json_elem['accreditor_url'] = accreditations['ACCURL']
+
+            text = get_eng_welsh_item('ACCTEXT', accreditations)
+            json_elem['text'] = text
+
+        if 'ACCDEPENDURL' in xml_elem or 'ACCDEPENDURLW' in xml_elem:
+            urls = get_eng_welsh_item('ACCDEPENDURL', xml_elem)
+            json_elem['url'] = urls
+
+        dependent_on = get_code_label_entry(xml_elem, lookup.accreditation_code, 'ACCDEPEND')
+        if dependent_on:
+            json_elem['dependent_on'] = dependent_on
+
+        acc_list.append(json_elem)
+
+    return acc_list
 
 def get_course_entry(accreditations, locations, locids, raw_inst_data,
                      raw_course_data, kisaims):
