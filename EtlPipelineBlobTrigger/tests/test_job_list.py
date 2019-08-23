0e8779937ce01a1e99abf21c1fdec504ef0e5406
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-08-23 15:07:18 +0100

    Add test for agg level lookup

--- EtlPipelineBlobTrigger/tests/test_job_list.py
+++ EtlPipelineBlobTrigger/tests/test_job_list.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-import xml.etree.ElementTree as ET
+import defusedxml.ElementTree as ET
 
 import xmltodict
 
@@ -16,9 +16,6 @@ class TestGetStats(unittest.TestCase):
         xml_string = get_string("fixtures/large-test-file.xml")
         root = ET.fromstring(xml_string)
         for institution in root.iter("INSTITUTION"):
-            raw_inst_data = xmltodict.parse(ET.tostring(institution))[
-                "INSTITUTION"
-            ]
             for course in institution.findall("KISCOURSE"):
                 raw_course_data = xmltodict.parse(ET.tostring(course))[
                     "KISCOURSE"
@@ -28,6 +25,20 @@ class TestGetStats(unittest.TestCase):
                 if len(json_job_list) > 1:
                     print(json.dumps(json_job_list, indent=4))
 
+
+class TestLookupDataFields(unittest.TestCase):
+    def setUp(self):
+        self.job_list = JobList()
+        self.lookup = self.job_list.data_fields_lookup
+
+    def test_agg_lookup(self):
+        xml_key = "COMAGG"
+        expected_key = "aggregation_level"
+        expected_elem_type = "M"
+        json_key = self.lookup[xml_key][0]
+        elem_type = self.lookup[xml_key][1]
+        self.assertEqual(expected_key, json_key)
+        self.assertEqual(expected_elem_type, elem_type)
 
 
 # TODO Test more of the functionality - more lookups etc
