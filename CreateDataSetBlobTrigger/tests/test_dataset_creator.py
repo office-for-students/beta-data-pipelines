2752099aed54d051a2978e3ffd4252839e2f35a7
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-08-30 11:57:47 +0100

    Add tests for time diffs 24 hours and over

--- CreateDataSetBlobTrigger/tests/test_dataset_creator.py
+++ CreateDataSetBlobTrigger/tests/test_dataset_creator.py
@@ -25,5 +25,26 @@ class TestGetTimeInMinutesSinceGivenDatetime(unittest.TestCase):
         expected_time_in_minutes = 120
         self.assertEqual(time_in_minutes, expected_time_in_minutes)
 
+    def test_get_time_in_minutes_since_given_datetime_with_one_day(self):
+        dt_now = datetime.now(timezone.utc)
+        dt_in_past = dt_now - timedelta(days=1)
+        time_in_minutes = get_time_in_minutes_since_given_datetime(dt_in_past)
+        expected_time_in_minutes = 24 * 60
+        self.assertEqual(time_in_minutes, expected_time_in_minutes)
+
+    def test_get_time_in_minutes_since_given_datetime_with_two_days(self):
+        dt_now = datetime.now(timezone.utc)
+        dt_in_past = dt_now - timedelta(days=2)
+        time_in_minutes = get_time_in_minutes_since_given_datetime(dt_in_past)
+        expected_time_in_minutes = 2 * 24 * 60
+        self.assertEqual(time_in_minutes, expected_time_in_minutes)
+
+    def test_get_time_in_minutes_since_given_datetime_with_seven_days(self):
+        dt_now = datetime.now(timezone.utc)
+        dt_in_past = dt_now - timedelta(days=7)
+        time_in_minutes = get_time_in_minutes_since_given_datetime(dt_in_past)
+        expected_time_in_minutes = 7 * 24 * 60
+        self.assertEqual(time_in_minutes, expected_time_in_minutes)
+
 
 # TODO add more tests
