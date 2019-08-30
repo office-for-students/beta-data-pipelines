11491df1b7805d4047c650bc939b27ff67adef5c
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-08-30 12:02:28 +0100

    Improve timezone handling
    Add 00:00 as offset from UTC to created_at so parsing string will
    automatically create a timezone aware datetime object.
    
    Move methods that don't need access to instance out of class.

--- CreateDataSetBlobTrigger/dataset_creator.py
+++ CreateDataSetBlobTrigger/dataset_creator.py
@@ -1,11 +1,9 @@
-import datetime
 import inspect
-import json
 import logging
-import pytz
 import os
 import sys
 
+from datetime import datetime, timezone
 from dateutil import parser
 
 CURRENTDIR = os.path.dirname(
@@ -37,7 +35,7 @@ class DataSetCreator:
 
     def has_enough_time_elaspsed_since_last_dataset_created(self):
         dt_of_latest_dataset_doc = self.get_datetime_of_latest_dataset_doc()
-        time_in_minutes_since_latest_dataset_doc = self.get_time_in_minutes_since_given_datetime(
+        time_in_minutes_since_latest_dataset_doc = get_time_in_minutes_since_given_datetime(
             dt_of_latest_dataset_doc
         )
         time_in_minutes_to_wait = int(
@@ -55,7 +53,9 @@ class DataSetCreator:
             self.cosmos_client.QueryItems(self.collection_link, query, options)
         )[0]
         created_at_str = latest_doc["created_at"]
-        return parser.isoparse(created_at_str)
+        dt_created_at = parser.isoparse(created_at_str)
+        dt_created_at.replace(tzinfo=timezone.utc)
+        return dt_created_at
 
     def get_max_version_number(self):
         query = "SELECT VALUE MAX(c.version) from c "
@@ -65,16 +65,11 @@ class DataSetCreator:
         )
         return max_version_number_list[0]
 
-    def get_time_in_minutes_since_given_datetime(self, dt_in_the_past):
-        dt_now = datetime.datetime.utcnow()
-        minutes_diff = round((dt_now - dt_in_the_past).total_seconds() / 60)
-        return minutes_diff
-
     def get_next_dataset_doc(self):
         next_version_number = self.get_next_dataset_version_number()
         dataset_doc = {}
         dataset_doc["builds"] = self.get_builds_value()
-        dataset_doc["created_at"] = datetime.datetime.utcnow().isoformat()
+        dataset_doc["created_at"] = datetime.now(timezone.utc).isoformat()
         dataset_doc["is_published"] = False
         dataset_doc["status"] = "in progress"
         dataset_doc["version"] = next_version_number
@@ -82,13 +77,10 @@ class DataSetCreator:
 
     def get_builds_value(self):
         builds = {}
-        builds["courses"] = self.get_initial_build_value()
-        builds["institutions"] = self.get_initial_build_value()
-        builds["search"] = self.get_initial_build_value()
+        builds["courses"] = get_initial_build_value()
+        builds["institutions"] = get_initial_build_value()
+        builds["search"] = get_initial_build_value()
         return builds
-
-    def get_initial_build_value(self):
-        return {"status": "pending"}
 
     def get_next_dataset_version_number(self):
         if self.get_number_of_dataset_docs() == 0:
@@ -102,3 +94,13 @@ class DataSetCreator:
             self.cosmos_client.QueryItems(self.collection_link, query, options)
         )
         return len(data_set_list)
+
+
+def get_time_in_minutes_since_given_datetime(dt_in_the_past):
+    dt_now = datetime.now(timezone.utc)
+    minutes_diff = round((dt_now - dt_in_the_past).total_seconds() / 60)
+    return minutes_diff
+
+
+def get_initial_build_value():
+    return {"status": "pending"}
