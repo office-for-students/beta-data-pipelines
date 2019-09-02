12c2145bab36a9fa043571e8b1031f06f37b60fe
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-09-02 12:53:20 +0100

    Add method to update the status

--- SharedCode/dataset_helper.py
+++ SharedCode/dataset_helper.py
@@ -28,5 +28,26 @@ class DataSetHelper:
         )
         return max_version_number_list[0]
 
-    def update_status(self, section, value):
-        pass
+    def update_status(self, item, value):
+        dataset_doc = self.get_latest_dataset_doc()
+        if item == "root":
+            dataset_doc["status"] = value
+        elif item == "courses":
+            dataset_doc["builds"]["courses"]["status"] = value
+        elif item == "institutions":
+            dataset_doc["builds"]["institutions"]["status"] = value
+        elif item == "search":
+            dataset_doc["builds"]["search"]["status"] = value
+        self.cosmos_client.UpsertItem(self.collection_link, dataset_doc)
+
+    def get_latest_dataset_doc(self):
+        max_version_number = self.get_latest_version_number()
+        query = f"SELECT * FROM c WHERE c.version = {max_version_number}"
+        options = {"enableCrossPartitionQuery": True}
+        return list(
+            self.cosmos_client.QueryItems(self.collection_link, query, options)
+        )[0]
+
+
+dsh = DataSetHelper()
+dsh.update_status("institutions", "in progress")
