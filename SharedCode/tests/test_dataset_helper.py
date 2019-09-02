381f1d23db313d613354d1bf5ed75d2bcaa5f2ee
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-09-02 16:33:29 +0100

    Add test for update_status

--- SharedCode/tests/test_dataset_helper.py
+++ SharedCode/tests/test_dataset_helper.py
@@ -5,7 +5,7 @@ from unittest import mock
 from SharedCode.dataset_helper import DataSetHelper
 
 
-class TestGetCollectionLink(unittest.TestCase):
+class TestDataSetHelper(unittest.TestCase):
     @mock.patch.dict(
         "os.environ",
         values={
@@ -16,12 +16,30 @@ class TestGetCollectionLink(unittest.TestCase):
     )
     @mock.patch("SharedCode.dataset_helper.get_cosmos_client")
     def test_initialisation(self, mock_get_cosmos_client):
+        mock_get_cosmos_client.return_value = mock.MagicMock()
         try:
-            dsh = DataSetHelper()
+            DataSetHelper()
         except:
             self.fail(
                 "DataSetHelper initialisation raised unexpected Exception"
             )
+
+    @mock.patch.dict(
+        "os.environ",
+        values={
+            "AzureCosmosDbDatabaseId": "test-db-id",
+            "AzureCosmosDbDataSetCollectionId": "test-dataset-collection-id",
+        },
+        clear=True,
+    )
+    @mock.patch("SharedCode.dataset_helper.get_cosmos_client")
+    def test_update_status(self, mock_get_cosmos_client):
+        dsh = DataSetHelper()
+        dsh.cosmos_client.UpsertItem = mock.MagicMock()
+        dsh.get_latest_doc = mock.MagicMock()
+        dsh.update_status("courses", "in progress")
+        dsh.get_latest_doc.assert_called_once()
+        dsh.cosmos_client.UpsertItem.assert_called_once()
 
 
 if __name__ == "__main__":
