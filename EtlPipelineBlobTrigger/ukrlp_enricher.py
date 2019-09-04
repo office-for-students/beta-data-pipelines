253ba2a4a0dbb2ac90782f63926603dd9e6ba5bd
phil@philratcliffe.co.uk <phil@philratcliffe.co.uk>
2019-09-04 13:01:47 +0100

    Formatting using Black with the config file in beta-data-pipeleines repo.

--- EtlPipelineBlobTrigger/ukrlp_enricher.py
+++ EtlPipelineBlobTrigger/ukrlp_enricher.py
@@ -11,12 +11,14 @@ class UkRlpCourseEnricher:
         """Takes a course and enriches ukprn names with UKRLP data"""
 
         ukprn = course["course"]["institution"]["ukprn"]
-        course["course"]["institution"]["ukprn_name"] = self.get_ukprn_name(ukprn)
+        course["course"]["institution"]["ukprn_name"] = self.get_ukprn_name(
+            ukprn
+        )
 
         pub_ukprn = course["course"]["institution"]["pub_ukprn"]
-        course["course"]["institution"]["pub_ukprn_name"] = self.get_ukprn_name(
-            pub_ukprn
-        )
+        course["course"]["institution"][
+            "pub_ukprn_name"
+        ] = self.get_ukprn_name(pub_ukprn)
 
     def get_ukprn_name(self, ukprn):
         """Returns a name for the ukprn"""
