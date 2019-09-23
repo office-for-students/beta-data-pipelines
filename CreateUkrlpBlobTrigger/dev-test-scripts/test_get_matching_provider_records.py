c8f927f5bc66ff22c5bf6accce6571ba9fcb6a1a
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-09-23 15:04:35 +0100

    Add dev script that gets a record for a UKPRN

---
+++ CreateUkrlpBlobTrigger/dev-test-scripts/test_get_matching_provider_records.py
@@ -0,0 +1,19 @@
+import inspect
+import json
+import os
+import sys
+
+
+CURRENTDIR = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe()))
+)
+PARENTDIR = os.path.dirname(CURRENTDIR)
+GRANDPARENTDIR = os.path.dirname(PARENTDIR)
+sys.path.insert(0, PARENTDIR)
+sys.path.insert(0, GRANDPARENTDIR)
+
+from ukrlp_client import UkrlpClient
+
+ukprn = "10004079"
+records = UkrlpClient.get_matching_provider_records(ukprn)
+print(json.dumps(records, indent=4))
