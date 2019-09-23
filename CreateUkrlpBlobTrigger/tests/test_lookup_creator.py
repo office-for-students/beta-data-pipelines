df1bcbadfff9644dd2491c24c02e74495086e4cf
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-09-23 10:33:04 +0100

    Add tests for title case needed

--- CreateUkrlpBlobTrigger/tests/test_lookup_creator.py
+++ CreateUkrlpBlobTrigger/tests/test_lookup_creator.py
@@ -1,11 +1,12 @@
 import json
 import unittest
 
+from unittest import mock
 from lookup_creator import LookupCreator
 from ukrlp_test_utils import get_string
 
 
-class TestGetMatchingProviderRecords(unittest.TestCase):
+class TestGetWebSite(unittest.TestCase):
     def test_get_website_100000794(self):
         matching_provider_records = json.loads(
             get_string("fixtures/ukrlp_10000794.json")
@@ -13,6 +14,26 @@ class TestGetMatchingProviderRecords(unittest.TestCase):
         expected_website = "www.boltoncollege.ac.uk"
         website = LookupCreator.get_website(matching_provider_records)
         self.assertEqual(expected_website, website)
+
+
+class TestNeedTitleCase(unittest.TestCase):
+    @mock.patch("lookup_creator.get_collection_link")
+    @mock.patch("lookup_creator.get_cosmos_client")
+    def test_title_case_needed_with_lipa(
+        self, mock_get_cosmos_client, mock_get_collection_link
+    ):
+        xml_string = get_string("fixtures/one_inst_one_course.xml")
+        lookup_creator = LookupCreator(xml_string)
+        self.assertFalse(lookup_creator.title_case_needed("LIPA"))
+
+    @mock.patch("lookup_creator.get_collection_link")
+    @mock.patch("lookup_creator.get_cosmos_client")
+    def test_title_case_needed_with_bexhill_college(
+        self, mock_get_cosmos_client, mock_get_collection_link
+    ):
+        xml_string = get_string("fixtures/one_inst_one_course.xml")
+        lookup_creator = LookupCreator(xml_string)
+        self.assertTrue(lookup_creator.title_case_needed("BEXHILL COLLEGE"))
 
 
 # TODO Test more of the functionality - more lookups etc
