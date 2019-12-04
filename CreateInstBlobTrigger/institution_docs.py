a3a38210731e908b1d9471e6a4797672c7e8a8f1
ponofrei <onofreipaul@gmail.com>
2019-12-04 19:51:32 +0200

    Added delay after bulk import to prevent CosmosDB timeout

--- CreateInstBlobTrigger/institution_docs.py
+++ CreateInstBlobTrigger/institution_docs.py
@@ -11,6 +11,7 @@ import inspect
 import logging
 import os
 import sys
+import time
 
 import defusedxml.ElementTree as ET
 
@@ -148,6 +149,7 @@ class InstitutionDocs:
                 # Reset values
                 new_docs = []
                 sproc_count = 0
+                time.sleep(2)
 
         if sproc_count > 0:
             logging.info(f"Begining execution of stored procedure for {sproc_count} documents")
