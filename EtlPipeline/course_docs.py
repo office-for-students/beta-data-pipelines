d2fa8fd4af4bb6ecdbca367fa7e450264213007f
Alan Warren <alan.warren@mobilise.cloud>
2020-11-11 15:08:09 +0000

    Display earnings aggregate heading information.

--- EtlPipeline/course_docs.py
+++ EtlPipeline/course_docs.py
@@ -402,6 +402,10 @@ def get_go_inst_json(raw_go_inst_data):
 
             go_salary["unavail_text_english"] = unavail_text_english
             go_salary["unavail_text_welsh"] = unavail_text_welsh         
+
+            if 'agg' in go_salary and 'subject' in go_salary:
+                go_salary["earnings_agg_unavail_message"] = get_earnings_agg_unavail_messages(go_salary["agg"], go_salary["subject"])
+
             go_salary_array.append(go_salary)
         else:
             for elem in raw_go_inst_data:
@@ -427,6 +431,10 @@ def get_go_inst_json(raw_go_inst_data):
 
                 go_salary["unavail_text_english"] = unavail_text_english
                 go_salary["unavail_text_welsh"] = unavail_text_welsh         
+
+                if 'agg' in go_salary and 'subject' in go_salary:
+                    go_salary["earnings_agg_unavail_message"] = get_earnings_agg_unavail_messages(go_salary["agg"], go_salary["subject"])
+
                 go_salary_array.append(go_salary)
     else: 
         # If no GO_SALARY node exists, we still need to display UNAVAIL text.
@@ -479,6 +487,10 @@ def get_leo3_inst_json(raw_leo3_inst_data):
 
             leo3["unavail_text_english"] = unavail_text_english
             leo3["unavail_text_welsh"] = unavail_text_welsh         
+
+            if 'agg' in leo3 and 'subject' in leo3:
+                leo3["earnings_agg_unavail_message"] = get_earnings_agg_unavail_messages(leo3["agg"], leo3["subject"])
+
             leo3_array.append(leo3)
         else:
             for elem in raw_leo3_inst_data:
@@ -514,7 +526,11 @@ def get_leo3_inst_json(raw_leo3_inst_data):
                 if 'LEO3PROV_PC_CF' in elem: leo3["inst_prov_pc_cf"] = elem["LEO3PROV_PC_CF"]
 
                 leo3["unavail_text_english"] = unavail_text_english
-                leo3["unavail_text_welsh"] = unavail_text_welsh         
+                leo3["unavail_text_welsh"] = unavail_text_welsh
+
+                if 'agg' in leo3 and 'subject' in leo3:
+                    leo3["earnings_agg_unavail_message"] = get_earnings_agg_unavail_messages(leo3["agg"], leo3["subject"])
+                    
                 leo3_array.append(leo3)
     else: 
         # If no LEO3 node exists, we still need to display UNAVAIL text.
@@ -1109,3 +1125,22 @@ def get_go_work_unavail_messages(xml_element_key, xml_agg_key, xml_unavail_reaso
         xml_unavail_reason_key,
     )
     return shared_utils.get_unavailable(raw_data_element)
+
+
+def get_earnings_agg_unavail_messages(agg_value, subject):
+    earnings_agg_unavail_messages = {}
+
+    if agg_value in ['21', '22']:
+        message_english = "The data displayed is from students on this and other "\
+            "courses in [Subject].\n\nThis includes data from this and related courses at the same university or "\
+            "college. There was not enough data to publish more specific information. This does not reflect on "\
+            "the quality of the course."
+        message_welsh = "Daw'r data a ddangosir gan fyfyrwyr ar y cwrs hwn a chyrsiau "\
+            "[Subject] eraill.\n\nMae hwn yn cynnwys data o'r cwrs hwn a chyrsiau cysylltiedig yn yr un brifysgol "\
+            "neu goleg. Nid oedd digon o ddata ar gael i gyhoeddi gwybodaeth fwy manwl. Nid yw hyn yn adlewyrchu "\
+            "ansawdd y cwrs."
+        
+        earnings_agg_unavail_messages['english'] = message_english.replace("[Subject]", subject['english_label'])
+        earnings_agg_unavail_messages['welsh'] = message_welsh.replace("[Subject]", subject['welsh_label'])
+
+    return earnings_agg_unavail_messages
