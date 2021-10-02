0150c5fd95e0efd6591991eb5f69467e7e02f674
SimonMobi <85573550+SimonMobi@users.noreply.github.com>
2021-10-02 13:22:19 +0100

    test url normalisation

---
+++ CreateInst/tests/test_ukrlp_contact_details.py
@@ -0,0 +1,41 @@
+import unittest
+
+from CreateInst.institution_docs import InstitutionProviderNameHandler
+from SharedCode.utils import normalise_url
+
+
+class TestGetStudentUnions(unittest.TestCase):
+
+    def test_normalised_url(self):
+        test_urls = [
+            "www.example.com",
+            "http://example.com",
+            "https://help.com",
+            "example.com"
+        ]
+
+        for line in test_urls:
+            result = normalise_url(line.rstrip())
+            print("result:", result)
+            self.assertTrue(result[0] == "h")
+            self.assertTrue(result[1] == "t")
+            self.assertTrue(result[2] == "t")
+            self.assertTrue(result[3] == "p")
+            self.assertTrue(result[4] == "s")
+            self.assertTrue(result[5] == ":")
+            self.assertTrue(result[6] == "/")
+            self.assertTrue(result[7] == "/")
+
+    def test_provider_name_formatted(self):
+        test_name = "an university and polYTech THAT for instance IN (and around) oF THE world"
+        expected_result = "An University and Polytech That for Instance in (And Around) of the World"
+        handler = InstitutionProviderNameHandler(white_list=[], welsh_uni_names=[])
+        result = handler.presentable(test_name)
+        self.assertEqual(result, expected_result, f"{result} should match {expected_result}")
+
+    def test_provider_name_on_whitelist(self):
+        test_name = "an university and polYTech THAT for instance IN (and around) oF THE world"
+        white_list = [test_name]
+        handler = InstitutionProviderNameHandler(white_list=white_list, welsh_uni_names=[])
+        result = handler.presentable(test_name)
+        self.assertEqual(result, test_name, f"{result} should match {test_name}")
