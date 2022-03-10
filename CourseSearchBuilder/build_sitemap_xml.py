91fc6c7a3124c5cd452a2fc801d2dc64497ec67e
Josh Ball <joshua.ball@mobilise.cloud>
2022-03-10 13:23:13 +0000

    fixed xml indentation for tests

--- CourseSearchBuilder/build_sitemap_xml.py
+++ CourseSearchBuilder/build_sitemap_xml.py
@@ -17,16 +17,17 @@ def build_xml_string(arg_list: tuple, xml: str) -> str:
     for args in arg_list:
         if len(args) == 4:
             centre_xml += f"""    <url>
-                    <loc>{build_course_details_url(*args)}></loc>
-                    <lastmod>{today}</lastmod>
-                </url>
-            """
+                <loc>{build_course_details_url(*args)}></loc>
+                <lastmod>{today}</lastmod>
+            </url>
+        """
         elif len(args) == 2:
             centre_xml += f"""    <url>
-                    <loc>{build_institution_details_url(*args)}></loc>
-                    <lastmod>{today}</lastmod>
-                </url>
-            """
+                <loc>{build_institution_details_url(*args)}></loc>
+                <lastmod>{today}</lastmod>
+            </url>
+        """
     final_xml = f"""{xml}
-            {centre_xml}</urlset>"""
+        {centre_xml}</urlset>"""
+    print(final_xml)
     return final_xml
--- CourseSearchBuilder/tests/text_site_map_xml.py
+++ CourseSearchBuilder/tests/text_site_map_xml.py
@@ -24,29 +24,31 @@ class TestSiteMapXml(unittest.TestCase):
 
     def test_xml_build(self):
         today = datetime.strftime(datetime.today(), "%Y-%m-%d")
-        xml = """<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"""
+        xml = """
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"""
         input = [
             ("10008071", "AAUNDERGRADUATE5YEAR", "Full-time", "en"),
             ("10008071", "AAUNDERGRADUATE5YEAR", "Full-time", "cy"),
             ("10008071", "en"),
             ("10008071", "cy"),
         ]
-        result = f"""<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                <url>
-                    <loc>https://discoveruni.gov.uk/en/course-details/10008071/AAUNDERGRADUATE5YEAR/Full-time/></loc>
-                    <lastmod>{today}</lastmod>
-                </url>
-                <url>
-                    <loc>https://discoveruni.gov.uk/cy/course-details/10008071/AAUNDERGRADUATE5YEAR/Full-time/></loc>
-                    <lastmod>{today}</lastmod>
-                </url>
-                <url>
-                    <loc>https://discoveruni.gov.uk/en/institution-details/10008071/></loc>
-                    <lastmod>{today}</lastmod>
-                </url>
-                <url>
-                    <loc>https://discoveruni.gov.uk/cy/institution-details/10008071/></loc>
-                    <lastmod>{today}</lastmod>
-                </url>
-            </urlset>"""
+        result = f"""
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url>
+                <loc>https://discoveruni.gov.uk/en/course-details/10008071/AAUNDERGRADUATE5YEAR/Full-time/></loc>
+                <lastmod>{today}</lastmod>
+            </url>
+            <url>
+                <loc>https://discoveruni.gov.uk/cy/course-details/10008071/AAUNDERGRADUATE5YEAR/Full-time/></loc>
+                <lastmod>{today}</lastmod>
+            </url>
+            <url>
+                <loc>https://discoveruni.gov.uk/en/institution-details/10008071/></loc>
+                <lastmod>{today}</lastmod>
+            </url>
+            <url>
+                <loc>https://discoveruni.gov.uk/cy/institution-details/10008071/></loc>
+                <lastmod>{today}</lastmod>
+            </url>
+        </urlset>"""
         self.assertEqual(build_xml_string(input, xml), result, "XML is not equal")
