18ddc43874f8bad1f79a752594db3ef119bdaf0b
nickpatti <nickdpatti50@gmail.com>
2023-09-19 16:40:23 +0100

    question 27 and 28 fix

--- EtlPipeline/course_stats.py
+++ EtlPipeline/course_stats.py
@@ -400,21 +400,10 @@ class Nss:
             if self.shared_utils.has_data(xml_elem):
                 json_elem.update(self.get_json_data(xml_elem))
             if self.shared_utils.need_unavailable(xml_elem):
-                if "nss_country_data" not in xml_elem:
-                    json_elem["unavailable"] = self.shared_utils.get_unavailable(
-                        xml_elem
-                    )
-            try:
-                if "nss_country_data" in xml_elem:
-                    nss_country_data = xml_elem["nss_country_data"]
-                    if type(nss_country_data) == list:
-                        for item in nss_country_data:
-                            json_elem.update(self.get_nss_country_question(item))
-                    else:
-                        json_elem.update(self.get_nss_country_question(nss_country_data))
-                json_elem_list.append(json_elem)
-            except TypeError as e:
-                logging.error(f"lists are not being handled correctly, {e}")
+                json_elem["unavailable"] = self.shared_utils.get_unavailable(
+                    xml_elem
+                )
+            json_elem_list.append(json_elem)
         return json_elem_list
 
 
