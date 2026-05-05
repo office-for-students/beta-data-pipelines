671589eb972b10a712065d1bc35d861196f33114
Simon Hurst <simon.hurst@mobilise.cloud>
2026-05-05 16:08:16 +0100

    - remove unused references left in after the last refactor

--- CourseSearchBuilder/__init__.py
+++ CourseSearchBuilder/__init__.py
@@ -1,28 +1,24 @@
-import os
 import logging
 import traceback
-
 from datetime import datetime
 
 import azure.functions as func
 
 from SharedCode import exceptions
 from SharedCode.dataset_helper import DataSetHelper
-# from SharedCode.mail_helper import MailHelper
-from SharedCode import utils
-
+from .build_institutions_json import build_institutions_json_files
 from .build_institutions_json import build_institutions_json_files
 from .build_search import build_search_index
+from .build_search import build_search_index
+from .build_sitemap_xml import build_sitemap_xml
 from .build_sitemap_xml import build_sitemap_xml
 from .build_subjects_json import build_subjects_json_file
+from .build_subjects_json import build_subjects_json_file
+from .build_version_json import build_version_json_file
 from .build_version_json import build_version_json_file
 
 
 def main(msgin: func.QueueMessage):
-    msgerror = ""
-    # mail_helper = MailHelper()
-    environment = os.environ["Environment"]
-
     dsh = DataSetHelper()
 
     try:
@@ -48,7 +44,8 @@ def main(msgin: func.QueueMessage):
             dsh.update_status("root", "succeeded")
         else:
             dsh.update_status("root", "failed")
-            raise exceptions.StopEtlPipelineErrorException(f"Build_complete status {build_complete}, All builds are not marked as succeeded")
+            raise exceptions.StopEtlPipelineErrorException(
+                f"Build_complete status {build_complete}, All builds are not marked as succeeded")
 
         function_end_datetime = datetime.today().strftime("%d-%m-%Y %H:%M:%S")
         function_end_date = datetime.today().strftime("%d.%m.%Y")
