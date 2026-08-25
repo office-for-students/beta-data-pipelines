32f40bbc294afaed52c890f8c0f7db05f1e0ebbe
nickmobilise <nicholas.cooper@mobilise.cloud>
2026-08-25 09:49:23 +0100

    fix: update sproc_count to 40
    - increase sproc_count to reduce 429 errors during bulk upload

--- EtlPipeline/course_docs.py
+++ EtlPipeline/course_docs.py
@@ -155,7 +155,7 @@ def load_course_docs(xml_string, version):
 
                 # course_count += 1
 
-                if sproc_count >= 20:
+                if sproc_count >= 40:
                     logging.info(f"Begining execution of stored procedure for {sproc_count} documents")
                     container.scripts.execute_stored_procedure(
                         sproc="bulkImport",
